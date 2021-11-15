a3340a9812790c357b91f752ff60e685775c04dc
Trent87 <jackofthewisps@gmail.com>
2021-11-15 07:53:42 -0800

    Rename update to 1.1.0
    I Win.... lol

--- config/slightguimodifications/cts.groovy
+++ config/slightguimodifications/cts.groovy
@@ -29,7 +29,7 @@ mainMenu {
             }
         }
 
-        text = literal("All of Fabric 4 - 1.0.2")
+        text = literal("All of Fabric 4 - 1.1.0")
     }
 
     button {
--- kubejs/startup_scripts/packversion.js
+++ kubejs/startup_scripts/packversion.js
@@ -1,1 +1,1 @@
-console.info('Loading All Of Fabric 4 v1.0.2')
+console.info('Loading All Of Fabric 4 v1.1.0')
